2a274bd42f05ae3063b6784fae2d4deab47614e5
KhaingWutYiWin <113814930+hoshikwyw@users.noreply.github.com>
2024-07-17 09:39:49 +0630

    List UI done

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,14 +1,28 @@
 import React from 'react'
+import { IoIosClose } from "react-icons/io";
+import UserInfo from './subComponents/UserInfo';
+
 
 const List = () => {
     return (
         <>
-           <label htmlFor="my-drawer" aria-label="close sidebar" className="drawer-overlay"></label>
-          <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
-            {/* Sidebar content here */}
-            <li><a>Sidebar Item 1</a></li>
-            <li><a>Sidebar Item 2</a></li>
-          </ul>
+            <label htmlFor="my-drawer" aria-label="close sidebar" className="drawer-overlay"></label>
+            <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
+                <p className=' w-full flex flex-row items-center justify-between'>
+                    <h1 className=' text-xl font-semibold'>Messages</h1>
+                    <label htmlFor="my-drawer" className="btn btn-ghost btn-primary drawer-button">
+                        <IoIosClose size={36} />
+                    </label>
+                </p>
+                <li className=' border-2 w-full overflow-hidden'>
+                    <UserInfo name="Halsey" avatar="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" lastMessage="Hello, how are you?" status="online" />
+                    <UserInfo name="John" lastMessage="dfjskdjgksjgjiefoijkl" status="offline" />
+                    <UserInfo name="Daniel Jame" avatar="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" lastMessage="Hello, how are you?" status="online" />
+                    <UserInfo name="Halsey" lastMessage="Hello, how are you?" status="online" />
+                    <UserInfo name="John" avatar="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" lastMessage="dfjskdjgksjgjiefoijkl" status="offline" />
+                    <UserInfo name="Daniel Jame" avatar="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" lastMessage="Hello, how are you?" status="online" />
+                </li>
+            </ul>
         </>
 
     )
