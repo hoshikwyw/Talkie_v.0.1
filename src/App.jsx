2de5d0b08aa4c3fef2386d5efa70d906fddb5bd2
KhaingWutYiWin <113814930+hoshikwyw@users.noreply.github.com>
2024-07-18 10:12:58 +0630

    Login&Register Ui done

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,11 @@
 import React from 'react'
-import List from './components/List'
-import Detail from './components/Detail'
-import Chat from './components/Chat'
-import Navbar from './components/subComponents/Navbar'
+import Paths from './components/Paths'
+
 
 const App = () => {
   return (
     <div data-theme="lemonade" className=' min-w-screen min-h-screen'>
-      <div className="drawer">
-        <input id="my-drawer" type="checkbox" className="drawer-toggle" />
-        <div className="drawer-content">
-          <Navbar />
-          <div className=" flex w-full mt-1">
-            <div className=" w-[70%] gap-2">
-              <Chat />
-            </div>
-            <div className=" w-[30%] border-s-2">
-              <Detail />
-            </div>
-          </div>
-        </div>
-        <div className="drawer-side">
-          <List />
-        </div>
-      </div>
+      <Paths />
     </div>
   )
 }
---
+++ src/components/Home.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import Navbar from './subComponents/Navbar'
+import Chat from './Chat'
+import Detail from './Detail'
+import List from './List'
+import Login from './Login'
+
+const Home = () => {
+    const user = false
+    return (
+        <div data-theme="lemonade" className=' min-w-screen min-h-screen'>
+            {user ? (
+                <div className="drawer">
+                    <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+                    <div className="drawer-content">
+                        <Navbar />
+                        <div className=" flex w-full mt-1">
+                            <div className=" w-[70%] gap-2">
+                                <Chat />
+                            </div>
+                            <div className=" w-[30%] border-s-2">
+                                <Detail />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="drawer-side">
+                        <List />
+                    </div>
+                </div>
+
+            ) : (
+                <Login />
+            )}
+        </div>
+    )
+}
+
+export default Home
